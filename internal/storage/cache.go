63ea066956d66dc0cbdbdfb87c12979ad181eac0
orenzhang <41963680+OrenZhang@users.noreply.github.com>
2026-04-10 18:08:21 +0800

    feat(cache): streamline local cache file handling and improve error management

--- internal/storage/cache.go
+++ internal/storage/cache.go
@@ -17,9 +17,11 @@ limitations under the License.
 package storage
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -69,7 +71,7 @@ func GetObjectViaCache(ctx context.Context, key string) (*ObjectInfo, error) {
 	key = strings.TrimPrefix(key, "/")
 	localPath := fmt.Sprintf(cacheFilePath, localCacheDir, key)
 	metaPath := fmt.Sprintf(cacheMetaFilePath, localCacheDir, key)
-	objInfo, err := GetLocalCacheFile(ctx, localPath, metaPath)
+	objInfo, err := getLocalCacheFile(ctx, localPath, metaPath)
 	if err != nil {
 		return nil, err
 	}
@@ -88,7 +90,7 @@ func GetObjectViaCache(ctx context.Context, key string) (*ObjectInfo, error) {
 		}
 
 		// 保存到本地
-		if err := SaveToLocalCache(ctx, localPath, metaPath, objInfo); err != nil {
+		if err := saveToLocalCache(ctx, localPath, metaPath, objInfo); err != nil {
 			return nil, err
 		}
 
@@ -102,7 +104,7 @@ func GetObjectViaCache(ctx context.Context, key string) (*ObjectInfo, error) {
 	return GetObjectViaCache(ctx, key)
 }
 
-func GetLocalCacheFile(ctx context.Context, localPath, metaPath string) (*ObjectInfo, error) {
+func getLocalCacheFile(ctx context.Context, localPath, metaPath string) (*ObjectInfo, error) {
 	ctx, span := otel_trace.Start(ctx, "S3.GetLocalCacheFile", trace.WithSpanKind(trace.SpanKindClient))
 	defer span.End()
 
@@ -118,8 +120,7 @@ func GetLocalCacheFile(ctx context.Context, localPath, metaPath string) (*Object
 	// 判断是否为其他异常
 	if err != nil {
 		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to open local cache file %s: %v", localPath, err)
-		return nil, LocalCacheError{}
+		return nil, err
 	}
 
 	// 读取元信息
@@ -133,22 +134,20 @@ func GetLocalCacheFile(ctx context.Context, localPath, metaPath string) (*Object
 	// 判断是否为其他异常
 	if err != nil {
 		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to read local cache meta file %s: %v", metaPath, err)
-		return nil, LocalCacheError{}
+		return nil, err
 	}
 
 	// 解析元信息
 	meta := &metaInfo{}
 	if err := json.Unmarshal(metaData, meta); err != nil {
 		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to parse local cache meta file %s: %v", metaPath, err)
-		return nil, LocalCacheError{}
+		return nil, err
 	}
 
 	return &ObjectInfo{CachePath: localPath, ContentLength: meta.ContentLength, ContentType: meta.ContentType}, nil
 }
 
-func SaveToLocalCache(ctx context.Context, localPath, metaPath string, objInfo *ObjectInfo) error {
+func saveToLocalCache(ctx context.Context, localPath, metaPath string, objInfo *ObjectInfo) error {
 	ctx, span := otel_trace.Start(ctx, "S3.SaveToLocalCache", trace.WithSpanKind(trace.SpanKindClient))
 	defer span.End()
 
@@ -156,90 +155,55 @@ func SaveToLocalCache(ctx context.Context, localPath, metaPath string, objInfo *
 	localDir := filepath.Dir(localPath)
 	if err := os.MkdirAll(localDir, 0755); err != nil {
 		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to create local cache directory %s: %v", localDir, err)
 		return err
 	}
 
-	// 创建临时文件
-	tempFile, err := os.CreateTemp(localDir, "cache_temp_*")
+	// 创建文件
+	if err := saveFile(localPath, objInfo.Body); err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		return err
+	}
+
+	// 创建元信息文件
+	meta := &metaInfo{ContentType: objInfo.ContentType, ContentLength: objInfo.ContentLength}
+	metaData, err := json.Marshal(meta)
 	if err != nil {
 		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to create temp file for local cache: %v", err)
+		return err
+	}
+	if err := saveFile(metaPath, bytes.NewReader(metaData)); err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func saveFile(localPath string, data io.Reader) error {
+	// 创建临时文件
+	tempFile, err := os.CreateTemp(filepath.Dir(localPath), "cache_temp_*")
+	if err != nil {
 		return err
 	}
 	defer os.Remove(tempFile.Name())
 
 	// 将内容写入临时文件
-	if _, err := tempFile.ReadFrom(objInfo.Body); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to write to temp file for local cache: %v", err)
+	if _, err := tempFile.ReadFrom(data); err != nil {
 		return err
 	}
 
 	// 确保数据写入磁盘
 	if err := tempFile.Sync(); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to sync temp file for local cache: %v", err)
 		return err
 	}
 
 	// 关闭临时文件
 	if err := tempFile.Close(); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to close temp file for local cache: %v", err)
-		return err
-	}
-
-	// 写入元信息
-	meta := &metaInfo{ContentType: objInfo.ContentType, ContentLength: objInfo.ContentLength}
-	metaData, err := json.Marshal(meta)
-	if err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to marshal meta info for local cache: %v", err)
-		return err
-	}
-
-	// 创建临时元信息文件
-	tempMetaFile, err := os.CreateTemp(localDir, "cache_meta_temp_*")
-	if err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to create temp meta file for local cache: %v", err)
-		return err
-	}
-	defer os.Remove(tempMetaFile.Name())
-
-	// 将元信息写入临时文件
-	if _, err := tempMetaFile.Write(metaData); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to write to temp meta file for local cache: %v", err)
-		return err
-	}
-
-	// 确保数据写入磁盘
-	if err := tempMetaFile.Sync(); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to sync temp meta file for local cache: %v", err)
-		return err
-	}
-
-	// 关闭临时元信息文件
-	if err := tempMetaFile.Close(); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to close temp meta file for local cache: %v", err)
 		return err
 	}
 
 	// 将临时文件重命名为最终文件
 	if err := os.Rename(tempFile.Name(), localPath); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to rename temp file to local cache file %s: %v", localPath, err)
-		return err
-	}
-
-	// 将临时元信息文件重命名为最终元信息文件
-	if err := os.Rename(tempMetaFile.Name(), metaPath); err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		logger.ErrorF(ctx, "Failed to rename temp meta file to local cache meta file %s: %v", metaPath, err)
 		return err
 	}
 
